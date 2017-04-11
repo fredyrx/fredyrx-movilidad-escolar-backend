ce1ec37b3ed7d0d1b829282cc211a26effb43dc5
= <=>
2017-04-11 10:21:03 -0500

    clean store repository

--- src/main/java/com/movilidadescolar/repo/StateRepository.java
+++ src/main/java/com/movilidadescolar/repo/StateRepository.java
@@ -1,11 +1,8 @@
 package com.movilidadescolar.repo;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.movilidadescolar.model.State;
-import com.movilidadescolar.model.User;
 
 public interface StateRepository extends JpaRepository<State,Long>{
 
