52e4424579594f1d2d461855bc9b06ceff1d324f
= <=>
2017-04-11 08:22:52 -0500

    add model state

---
+++ src/main/java/com/movilidadescolar/model/State.java
@@ -0,0 +1,56 @@
+package com.movilidadescolar.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "states")
+public class State {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "order_number")
+	private Integer orderNumber;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getOrderNumber() {
+		return orderNumber;
+	}
+
+	public void setOrderNumber(Integer orderNumber) {
+		this.orderNumber = orderNumber;
+	}
+
+	protected State(){
+		// Will be use by JPA
+	}
+	
+	public State(String name, Integer orderNumber){
+		this.name = name;
+		this.orderNumber = orderNumber;
+	}
+}
